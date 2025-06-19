a4ddbfbfc4a9bc6803fb6dbd8e96adcc64b0fddb
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2025-06-19 20:47:24 +0200

    Install lit, add loader

---
+++ v-src/design/components/loader/loader.js
@@ -0,0 +1,26 @@
+import { html, css, LitElement } from '/v-src/modules/lit-core.min.js'
+
+export class Loader extends LitElement {
+    static styles = css`
+        @keyframes rotating {
+            to {
+                transform: rotate(360deg);
+            }
+        }
+
+        .loader {
+            animation-name: rotating;
+            animation-duration: 1s;
+            animation-timing-function: ease;
+            animation-iteration-count: infinite;
+        }
+    `;
+
+    render() {
+        return html`
+            <image src="/v-src/assets/palette.svg" alt="loading" class="loader" />
+        `;
+    }
+}
+
+customElements.define('l-loader', Loader);
