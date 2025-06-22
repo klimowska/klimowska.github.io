638d4188811f78cdac85b6fb837a63146d5ddaae
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2025-06-22 13:58:02 +0200

    Modify imports

--- v-src/vendor/import-aframe.js
+++ v-src/vendor/import-aframe.js
@@ -1,6 +1,10 @@
 import { createVendorScript } from '/v-src/lib/createVendorScript.js';
 
-const SRC = 'https://cdnjs.cloudflare.com/ajax/libs/aframe/1.7.0/aframe.min.js';
-const INTEGRITY = 'sha512-QRe0tMdbQUsh9BJOh+ijnsJaLoO68ku2a9/7HmQCIvqpeWRkqc4Ee0Ohjvt6y7Fx/ShFTLoL/H3ZgxZrXB7ERA==';
+// const SRC = 'https://cdnjs.cloudflare.com/ajax/libs/aframe/1.7.0/aframe.min.js';
+// const INTEGRITY = 'sha512-QRe0tMdbQUsh9BJOh+ijnsJaLoO68ku2a9/7HmQCIvqpeWRkqc4Ee0Ohjvt6y7Fx/ShFTLoL/H3ZgxZrXB7ERA==';
+
+const SRC = 'https://cdn.jsdelivr.net/gh/aframevr/aframe@1.4.0/aframe/dist/aframe-v1.4.0.js';
+const INTEGRITY = 'sha384-fxhhFblRQEyLTZTLdKeMxcK2QNlvGKAtjxTt9vemHGRx5TH7dMM8Qh0vp5rBCxs9';
+
 
 export const importAframe = () => createVendorScript(SRC, INTEGRITY);
